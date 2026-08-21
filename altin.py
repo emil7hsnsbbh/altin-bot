f609a80cb47cd318de37eb3541142fc3beb96177
emil7hsnsbbh <em1lbspm@gmail.com>
2026-08-21 15:40:01 +0300

    Update altin.py

--- altin.py
+++ altin.py
@@ -1,1 +1,23 @@
-print("Bot çalıştı")
+import os
+import requests
+
+TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = "6142462229"
+
+mesaj = """
+💰 Gram Altın
+
+Test mesajı
+
+🕒 İlk kurulum başarılı
+"""
+
+requests.post(
+    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": mesaj
+    }
+)
+
+print("Mesaj gönderildi")
