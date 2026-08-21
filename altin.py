9a2cbdaa697d7c3fd0a4ecfa5819f3aeab2a7899
emil7hsnsbbh <em1lbspm@gmail.com>
2026-08-21 15:50:01 +0300

    Update altin.py

--- altin.py
+++ altin.py
@@ -1,15 +1,31 @@
 import os
 import requests
+from datetime import datetime
 
 TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = "6142462229"
 
-mesaj = """
+# Gram altın verisi
+r = requests.get(
+    "https://api.genelpara.com/json/?list=altin&sembol=GA"
+)
+
+data = r.json()
+
+gram = data["GA"]
+
+fiyat = gram["satis"]
+degisim = gram["degisim"]
+
+tarih = datetime.now().strftime("%d.%m.%Y")
+
+mesaj = f"""
+📅 {tarih}
+
 💰 Gram Altın
+{fiyat} TL
 
-Test mesajı
-
-🕒 İlk kurulum başarılı
+📈 Günlük Değişim: %{degisim}
 """
 
 requests.post(
